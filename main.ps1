fc46650e82bf8f8b6e763ad6a3422c6794bba04f
skilson <seth.kilson@gmail.com>
2021-04-13 23:38:33 -0500

    ants and springs

--- main.ps1
+++ main.ps1
@@ -15,30 +15,39 @@ do {
     Write-Host " [R]: to Reset collection"
     Write-Host " [Q]: to Quit`n"
 
-    $selection = Read-Host
+    [string]$selection = Read-Host
 
     switch ($selection) {
-        { (Confirm-IfInteger -ValueToTest $_) -and ([int]$_ -gt 0) -and ([int]$_ -le $farm.Chickens.Count) } {
+        { !($_.StartsWith("+")) -and !($_.StartsWith("-")) -and (Confirm-IfInteger -ValueToTest $_) -and ([int]$_ -gt 0) -and ([int]$_ -le $farm.Chickens.Count) } {
             $selection = $selection - 1
             Get-Mating -Breed $farm.Chickens[$selection].Breed -Farm $farm
             Get-Matings -Breed $farm.Chickens[$selection].Breed -Farm $farm
+            Break
         }
         { $_.StartsWith("+") } {
-            $target = $selection.Replace("+", "")
+            $target = [string]$selection.Replace("+", "")
             if ((Confirm-IfInteger -ValueToTest $target) -and ([int]$target -gt 0) -and ([int]$target -le $farm.Chickens.Count)) {
                 Add-ToCollection -NewBreed $farm.Chickens[$target - 1].Breed
             }
             else { Write-Host "Problem with the input given." -ForegroundColor Red }
+            Break
         }
         { $_.StartsWith("-") } {
-            $target = $selection.Replace("-", "")
+            $target = [string]$selection.Replace("-", "")
             if ((Confirm-IfInteger -ValueToTest $target) -and ([int]$target -gt 0) -and ([int]$target -le $farm.Chickens.Count)) {
                 Remove-FromCollection -RemoveBreed $farm.Chickens[$target - 1].Breed
             }
             else { Write-Host "Problem with the input given." -ForegroundColor Red }
+            Break
         }
-        "R" { Clear-Collection }
-        "Q" { Write-Host "Thanks for stopping by!" -ForegroundColor Cyan }
+        "R" {
+            Clear-Collection
+            Break
+        }
+        "Q" {
+            Write-Host "Thanks for stopping by!" -ForegroundColor Cyan
+            Break
+        }
         Default {
             Write-Host "Not sure what that is.. Please try again or Q to quit." -ForegroundColor Red
         }
